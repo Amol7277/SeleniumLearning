828ffcd13aeac9787f0788c54cc7182d188a6cb4
Amol <amolsonar1997@gmail.com>
2025-12-29 18:11:57 +0530

    Initial commit 29/12/2025

---
+++ src/ex01_selenium_basics/test_selenium_01.py
@@ -0,0 +1,13 @@
+from selenium import webdriver
+import pytest
+import allure
+
+
+@allure.title("Verify that we are able to open a page by using Selenium.")
+@allure.description("We will open a page and verify that it is getting opened by using Selenium.")
+def test_first_tc():
+    driver = webdriver.Chrome()
+    driver.get("https://thetestingacademy.com")
+    driver.maximize_window()
+    print(driver.title)
+    assert driver.title == "TheTestingAcademy | Learn Software Testing and Automation Testing"
